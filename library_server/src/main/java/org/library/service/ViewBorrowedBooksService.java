47753fc08f98481c7b1aa9b81a81e7d1ac51869d
vidved123 <76038269+vidved123@users.noreply.github.com>
2025-06-16 12:05:20 +0530

    Update ViewBorrowedBooksService.java

--- library_server/src/main/java/org/library/service/ViewBorrowedBooksService.java
+++ library_server/src/main/java/org/library/service/ViewBorrowedBooksService.java
@@ -1,16 +1,16 @@
 package org.library.service;
 
-import org.library.dto.ViewBorrrowedBooksdto;
+import org.library.dto.ViewBorrowedBooksdto;
 
 import java.util.List;
 
 public interface ViewBorrowedBooksService {
 
-    List<ViewBorrrowedBooksdto> getBorrowedBooks(Long currentUserId, String role);
+    List<ViewBorrowedBooksdto> getBorrowedBooks(Long currentUserId, String role);
 
     // Method to search borrowed books by userId for admins
-    List<ViewBorrrowedBooksdto> searchBorrowedBooksByUserId(Long userId);
+    List<ViewBorrowedBooksdto> searchBorrowedBooksByUserId(Long userId);
 
     // Method to search borrowed books by title or author (for normal users)
-    List<ViewBorrrowedBooksdto> searchBorrowedBooks(String searchQuery, Long userId, String role);
+    List<ViewBorrowedBooksdto> searchBorrowedBooks(String searchQuery, Long userId, String role);
 }
