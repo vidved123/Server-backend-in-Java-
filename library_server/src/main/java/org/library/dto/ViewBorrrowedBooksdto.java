722d87338897d2a4a44ed611b3b310bccd29fa2b
vidved123 <76038269+vidved123@users.noreply.github.com>
2025-06-16 11:46:52 +0530

    Update ViewBorrrowedBooksdto.java

--- library_server/src/main/java/org/library/dto/ViewBorrrowedBooksdto.java
+++ library_server/src/main/java/org/library/dto/ViewBorrrowedBooksdto.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Setter
 @Getter
-public class ViewBorrrowedBooksdto {
+public class ViewBorrowedBooksdto {
     private Long bookId;
     private String title;
     private String author;
