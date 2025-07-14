f6ba2f8705e1114ff6136281ad279abf965c0f23
vidved123 <76038269+vidved123@users.noreply.github.com>
2025-07-14 15:17:28 +0530

    Update WebConfig.java

--- library_server/src/main/java/library/org/library/config/WebConfig.java
+++ library_server/src/main/java/library/org/library/config/WebConfig.java
@@ -4,9 +4,21 @@ import org.library.filter.RateLimitFilter;
 import org.library.service.RateLimitService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations(
+                "file:/Users/vidhyut/Desktop/Prathisthan Projects/Server-Backend-java/library-images/",
+                "classpath:/static/images/"
+            );
+    }
 
     @Bean
     public RateLimitFilter rateLimitingFilter(RateLimitService rateLimitService) {
